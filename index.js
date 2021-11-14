42818fcf224cd1c645286d1e9c408bc96a963845
douglassmoss <douglassmoss@icloud.com>
2021-11-13 17:12:49 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main");
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+element.id = "victory";
+element.innerHTML = "Dexter is the champion";
